a4a01cd694dcaf87c65f612fa0e8a9101520a7f2
Bucky Wolfe <igneous@usleep.consulting>
2019-02-10 15:16:29 +0700

    Successful chat sending

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -2,7 +2,7 @@
 
 function checkLocation() {
   lreq = new XMLHttpRequest();
-  lreq.open('GET', 'https://usleep-chat.herokuapp.com/loc.json', true);
+  lreq.open('GET', 'http://localhost:9292/loc.json', true);
   lreq.send();
   lreq.onreadystatechange = updateLocTime;
 }
@@ -12,7 +12,8 @@ function updateLocTime() {
     loc = JSON.parse(lreq.responseText);
     document.getElementById('location').innerHTML = "Location: " + loc.location;
     var updateTime = function() {
-      document.getElementById('time').innerHTML = luxon.DateTime.local().setZone(loc.timezone).toFormat("cccc LLL d, h:mm:ss a (ZZZ)");
+      document.getElementById('time').innerHTML =
+        luxon.DateTime.local().setZone(loc.timezone).toFormat("cccc LLL d, h:mm:ss a (ZZZ)");
     }
     setInterval(updateTime, 1000);
   }
@@ -20,7 +21,7 @@ function updateLocTime() {
 
 function checkChatStatus() {
   sreq = new XMLHttpRequest();
-  sreq.open('GET', 'https://usleep-chat.herokuapp.com/status.json', true);
+  sreq.open('GET', 'http://localhost:9292/status.json', true);
   sreq.send();
   sreq.onreadystatechange = updateChatStatus;
 }
@@ -44,14 +45,108 @@ function updateChatStatus() {
 
     if (response.chat_status != "offline") {
       document.getElementById('status').innerHTML =
-        "Status: <font color='" + color + "'>" + response.chat_status + "</font> " +
-        "[<a href='#' onclick='showChat()'>Chat</a>]";
+        "Chat: <font color='" + color + "'>" + response.chat_status + "</font> " +
+        "[<a href='#' onclick='showChat()'>open livechat</a>]";
     } else {
       document.getElementById('status').innerHTML =
-        "Status: <font color='" + color + "'>" + response.chat_status + "</font>";
+        "Chat: <font color='" + color + "'>" + response.chat_status + "</font>";
     }
   }
 }
 
-function sendMessage(msg) {
+function sendMessage() {
+  postdata = {};
+  postdata.time = new Date();
+  postdata.user = getCookie("username");
+  postdata.message = document.getElementById("chatmsg").value;
+  mreq = new XMLHttpRequest();
+  mreq.open('POST', 'http://localhost:9292/message.json', true);
+  mreq.setRequestHeader("Content-Type", "application/json");
+  mreq.send(JSON.stringify(postdata));
+  mreq.onreadystatechange = updateChatStatus;
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  var username = getCookie("username");
+  if (username != "") {
+  } else {
+    username = prompt("Please enter your name:", "");
+    if (username != "" && username != null) {
+      setCookie("username", username, 7);
+    }
+  }
+}
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function showChat() {
+  checkCookie();
+  document.getElementById('chat').style.display = "block";
+  dragElement(document.getElementById('chat'));
+}
+
+function hideChat() {
+  document.getElementById('chat').style.display = "none";
+}
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV: 
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
